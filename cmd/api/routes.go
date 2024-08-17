ad0b255c9beffdb6565f5508aaaeebc54fe64bbd
Michael Wilburn <michael-mw@hotmail.com>
2024-08-17 12:26:18 -0300

    Choosing a router and encapsulating the API routes

---
+++ cmd/api/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() *httprouter.Router {
+	// Initialize a new httprouter router instance.
+	router := httprouter.New()
+
+	/*Register the relevant methods, URL patterns and handler functions for our
+	endpoints using HandleFunc() method. Note that http.MethodGet and http.MethodPost
+	are constants which equate to the string "GET" and "POST" respectively.*/
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/cars", app.crateCarHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/cars/:id", app.showCarHandler)
+
+	// Return the httprouter instance
+	return router
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Michael-Wilburn/go_api_car
 
 go 1.22.1
+
+require github.com/julienschmidt/httprouter v1.3.0
