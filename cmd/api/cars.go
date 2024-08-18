1972f40e15f59c8897f03b8ef027d57f6e1ef4a6
Michael Wilburn <michael-mw@hotmail.com>
2024-08-18 19:18:20 -0300

    Sending custom Error messages

--- cmd/api/cars.go
+++ cmd/api/cars.go
@@ -18,7 +18,7 @@ func (app *application) createCarHandler(w http.ResponseWriter, r *http.Request)
 func (app *application) showCarHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil || id == uuid.Nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -46,7 +46,6 @@ func (app *application) showCarHandler(w http.ResponseWriter, r *http.Request) {
 	// Encode the struct to JSON and send it as the HTTP response.
 	err = app.writeJSON(w, http.StatusOK, envelope{"car": car}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
