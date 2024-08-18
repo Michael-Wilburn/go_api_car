56373e66e7bc820d5febedc70a99911f8ad8d1c3
Michael Wilburn <michael-mw@hotmail.com>
2024-08-18 15:14:29 -0300

    Changing keys in the JSON object

--- internal/data/cars.go
+++ internal/data/cars.go
@@ -7,18 +7,18 @@ import (
 )
 
 type Car struct {
-	ID         uuid.UUID
-	Online     bool
-	CarType    string
-	Brand      string
-	Model      string
-	Year       int32
-	Kilometers int64
-	CarDomain  string
-	Price      float64
-	InfoPrice  float64
-	Currency   string
-	ChasisCode string
-	MotorCode  string
-	CreatedAt  time.Time
+	ID         uuid.UUID `json:"id"`
+	Online     bool      `json:"online"`
+	CarType    string    `json:"car_type"`
+	Brand      string    `json:"brand"`
+	Model      string    `json:"model"`
+	Year       int32     `json:"year"`
+	Kilometers int64     `json:"kilometers"`
+	CarDomain  string    `json:"car_domain"`
+	Price      float64   `json:"price"`
+	InfoPrice  float64   `json:"info_price"`
+	Currency   string    `json:"currency"`
+	ChasisCode string    `json:"chasis_code"`
+	MotorCode  string    `json:"motor_code"`
+	CreatedAt  time.Time `json:"created_at"`
 }
